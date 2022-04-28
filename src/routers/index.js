c347fd69513a385e5c068457559caed80eb048b7
siraom15 <siriwatjaiyungyuen@gmail.com>
2022-04-28 19:32:27 +0700

    Add Schedules page

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,5 +1,6 @@
 import { createWebHashHistory, createRouter } from 'vue-router';
 import Home from '../views/Home.vue';
+import Schedules from '../views/Schedules.vue';
 
 const history = createWebHashHistory();
 const routes = [
@@ -7,6 +8,11 @@ const routes = [
     path: '/',
     name: 'home',
     component: Home,
+  },
+  {
+    path: '/schedules',
+    name: 'schedules',
+    component: Schedules,
   },
 ];
 
