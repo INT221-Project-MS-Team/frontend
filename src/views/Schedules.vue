8e6da46d1c0151ae57ce021a14a1e53e952fdca4
siraom15 <siriwatjaiyungyuen@gmail.com>
2022-04-28 21:40:48 +0700

    Add Fetch with Backend

--- src/views/Schedules.vue
+++ src/views/Schedules.vue
@@ -1,14 +1,25 @@
 <script setup>
 import { ref } from '@vue/reactivity';
+import { onBeforeMount } from '@vue/runtime-core';
 
 const schedules = ref([]);
+
+const getSchedules = async () => {
+  const response = await fetch(
+    import.meta.env.VITE_SERVER_URI + '/api/events'
+  );
+  const data = await response.json();
+  schedules.value = data;
+  console.log(data);
+};
+onBeforeMount(async () => {
+  await getSchedules();
+});
 </script>
 
 <template>
   <div class="bg-schedules-bg w-screen h-screen bg-no-repeat bg-cover">
-    <div>
-      
-    </div>
+    <div></div>
   </div>
 </template>
 
