e590d1c04655ed9ccd811a008f377eeb38318765
siraom15 <siriwatjaiyungyuen@gmail.com>
2022-04-28 19:32:15 +0700

    Add Button components

---
+++ src/components/Button.vue
@@ -0,0 +1,30 @@
+<script setup>
+const props = defineProps({
+  text: {
+    type: String,
+    default: '',
+  },
+  btnType: {
+    type: String,
+    default: 'primary',
+  },
+});
+const btnTypes = {
+  primary: ['bg-white', 'text-clinic-blue-300', 'hover:bg-slate-100 '],
+  secondary: ['bg-transparent', 'text-white', 'hover:bg-white', 'hover:text-clinic-blue-300'],
+};
+</script>
+
+<template>
+  <div>
+    <button
+      type="button"
+      class="font-bold border-2 focus:outline-none rounded-3xl text-lg px-10 py-3 text-center mr-2 mb-2 uppercase"
+      :class="[btnTypes[btnType]]"
+    >
+      {{ text }}
+    </button>
+  </div>
+</template>
+
+<style scoped></style>
