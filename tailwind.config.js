93a65bfe3144b3beb72ab5e5528009ba02f2e185
siraom15 <siriwatjaiyungyuen@gmail.com>
2022-04-28 18:15:11 +0700

    Update Css & Font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-    "./node_modules/flowbite/**/*.js"
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}',
+    './node_modules/flowbite/**/*.js',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        prompt: ['Prompt', 'sans-serif'],
+        kanit: ['Kanit', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
-}
+};
