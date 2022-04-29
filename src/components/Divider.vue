94a109959fa463200631e30136e88453a6125922
siraom15 <siriwatjaiyungyuen@gmail.com>
2022-04-29 16:37:36 +0700

    Add divider component

---
+++ src/components/Divider.vue
@@ -0,0 +1,17 @@
+<script setup>
+const props = defineProps({
+  text: {
+    type: String,
+    default: '',
+  },
+});
+</script>
+
+<template>
+  <div class="relative flex py-5 items-center">
+    <span class="flex-shrink mx-4 text-gray-400">{{ text }}</span>
+    <div class="flex-grow border-t border-gray-400"></div>
+  </div>
+</template>
+
+<style scoped></style>
