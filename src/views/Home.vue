268874314849d174d664098f5fc19164d1f51aba
siraom15 <siriwatjaiyungyuen@gmail.com>
2022-04-29 14:21:39 +0700

    Format Code

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,14 +3,16 @@ import Button from '../components/Button.vue';
 </script>
 
 <template>
-  <div class="bg-home-bg w-screen h-screen bg-no-repeat bg-cover bg-center flex items-center">
+  <div
+    class="bg-home w-screen h-screen bg-no-repeat bg-cover bg-center flex items-center"
+  >
     <div class="container mx-auto px-4 pt-16">
       <div class="flex flex-wrap-reverse justify-center items-center gap-3">
         <div class="flex flex-col gap-6">
-          <p class="text-7xl text-white font-black  uppercase tracking-widest">
+          <p class="text-7xl text-white font-black uppercase tracking-widest">
             Booking
           </p>
-          <p class="text-2xl text-white  font-medium  tracking-wider">
+          <p class="text-2xl text-white font-medium tracking-wider">
             Check for available and <br />
             make your reservation
           </p>
@@ -21,7 +23,11 @@ import Button from '../components/Button.vue';
             <Button text="Reserve !" btnType="secondary" />
           </div>
         </div>
-        <img class="calender bounce-2 object-cover w-3/5" src="images/calen-phone-new.png" alt="cover" />
+        <img
+          class="calender bounce-2 object-cover w-3/5"
+          src="images/calen-phone-new.png"
+          alt="cover"
+        />
       </div>
     </div>
   </div>
@@ -29,11 +35,10 @@ import Button from '../components/Button.vue';
 
 <style scoped>
 .calender {
-  align-self: flex-end;
   animation-duration: 2s;
   animation-iteration-count: infinite;
   transform-origin: bottom;
-  filter: drop-shadow(.5rem .5rem 1rem #8da1ff);
+  filter: drop-shadow(0.5rem 0.5rem 1rem #8da1ff);
 }
 
 .bounce-2 {
@@ -62,5 +67,4 @@ import Button from '../components/Button.vue';
     transform: translateY(0);
   }
 }
-
 </style>
