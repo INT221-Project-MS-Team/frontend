9badf364c14ff5ed6fe63ce6cf3f25f8dc81c2c1
bewburats <bew25442@gmail.com>
2022-04-29 23:42:56 +0700

    edit images size

--- src/components/NoEvent.vue
+++ src/components/NoEvent.vue
@@ -2,7 +2,7 @@
 
 <template>
   <div class="flex flex-col items-center justify-center">
-    <img class="object-cover w-4/5" src="/images/girl.png" alt="cover" />
+    <img class="object-cover w-2/5" src="/images/girl-black.png" alt="cover" />
     <p class="text-gray-400 text-sm md:text-lg lg:text-2xl">
       No Scheduled Events
     </p>
