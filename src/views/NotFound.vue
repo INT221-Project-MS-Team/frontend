05473d535e27df1fd507408a29fac2df29d6067a
bewburats <bew25442@gmail.com>
2022-04-29 23:43:10 +0700

    edit css

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -10,17 +10,12 @@ const goBack=()=>myRouter.go(-1);
     class="bg-home w-screen h-screen bg-no-repeat bg-cover bg-center flex items-center"
   >
     <div class="container mx-auto px-4 pt-16">
-      <div class="flex flex-col justify-center items-center gap-3">
-        <img
-          class="calender bounce-2 object-cover w-5/12"
-          src="/images/cover-model-404.png"
-          alt="404"
-        />
+      <div class="flex flex-wrap-reverse justify-center items-center gap-3">
         <div class="flex flex-col gap-6">
           <p class="text-5xl text-white font-black uppercase tracking-widest">
             404 PAGE NOT FOUND
           </p>
-          <p class="text-2xl text-white font-medium tracking-wider">
+          <p class="text-xl text-white font-medium tracking-wider">
             It's an error .<br />
              The request url was not found on this.
           </p>
@@ -28,10 +23,13 @@ const goBack=()=>myRouter.go(-1);
             <router-link :to="{ name: 'NotFound' }">
               <Button  text="Go back !" btnType="secondary"  @click="goBack" />
             </router-link>
-            
           </div>
         </div>
-        
+         <img
+          class="calender bounce-2 object-cover w-5/12"
+          src="/images/cover-model-404.png"
+          alt="404"
+        />
       </div>
     </div>
   </div>
