2c615dc5045e4cffa86151d9a6347b97f84526c4
siraom15 <siriwatjaiyungyuen@gmail.com>
2022-04-29 12:23:45 +0700

    change format

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./routers";
-import "./index.css";
-import "flowbite";
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './routers';
+import './index.css';
+import 'flowbite';
 
 const app = createApp(App);
 app.use(router);
-app.mount("#app");
+app.mount('#app');
