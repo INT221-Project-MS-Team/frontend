f310da36ef2deeb03bff24dfd753bc1313a29dde
siraom15 <siriwatjaiyungyuen@gmail.com>
2022-05-03 00:10:53 +0700

    Fix Bug when filter data

--- src/views/Schedules.vue
+++ src/views/Schedules.vue
@@ -4,7 +4,7 @@ import EventCard from '../components/EventCard.vue';
 import SmButton from '../components/SmButton.vue';
 import Divider from '../components/Divider.vue';
 import LitepieDatepicker from 'litepie-datepicker';
-import { getCurrentDateTime, convertToISO } from '../utils';
+import { getCurrentDateTime, convertDateFormat } from '../utils';
 import { SearchIcon } from '@heroicons/vue/outline';
 import { ref } from '@vue/reactivity';
 import { computed, onBeforeMount } from '@vue/runtime-core';
@@ -13,6 +13,8 @@ const schedulesData = ref([]);
 const categoriesData = ref([]);
 const selectedEventCategoryName = ref('All');
 const selectedDate = ref('');
+const allEventStatus = ref(['All', 'Upcoming', 'Past']);
+const selectedEventStatus = ref('All');
 const filterBookingNameEmail = ref('');
 const sortBy = ref('eventStartTime');
 const sortOrder = ref('desc');
@@ -30,49 +32,63 @@ const endPointUrl = computed(() => {
 
 const filteredSchedules = computed(() => {
   const schedules = schedulesData.value;
+  let result = schedules;
   const categoryName =
     selectedEventCategoryName.value === 'All'
       ? null
       : selectedEventCategoryName.value;
-  const dateTemp = selectedDate.value === '' ? null : selectedDate.value;
+  const date = selectedDate.value === '' ? null : selectedDate.value;
   const bookingNameEmail = filterBookingNameEmail.value;
+  const eventStatus =
+    selectedEventStatus.value === 'All' ? null : selectedEventStatus.value;
 
-  if (categoryName && dateTemp && bookingNameEmail) {
-    return schedules.filter(
-      (schedule) =>
-        schedule.eventCategory.eventCategoryName === categoryName &&
-        schedule.eventStartTime.includes(convertToISO(dateTemp)) &&
-        (schedule.bookingName.includes(bookingNameEmail) ||
-          schedule.bookingEmail.includes(bookingNameEmail))
-    );
-  }
-
+  // filter categoryName
   if (categoryName) {
-    return schedules.filter(
-      (schedule) => schedule.eventCategory.eventCategoryName === categoryName
-    );
+    result = result.filter((schedule) => {
+      return schedule.eventCategory.eventCategoryName === categoryName;
+    });
   }
 
-  if (dateTemp) {
-    return schedules.filter((schedule) =>
-      schedule.eventStartTime.includes(convertToISO(dateTemp))
-    );
+  // filter date
+  if (date) {
+    result = result.filter((schedule) => {
+      return schedule.eventStartTime.includes(convertDateFormat(date));
+    });
   }
 
-  if(bookingNameEmail){
-    return schedules.filter((schedule) =>
-      (schedule.bookingName.includes(bookingNameEmail) ||
-        schedule.bookingEmail.includes(bookingNameEmail))
-    );
+  // filter bookingNameEmail
+  if (bookingNameEmail) {
+    result = result.filter((schedule) => {
+      return (
+        schedule.bookingName
+          .toLowerCase()
+          .includes(bookingNameEmail.toLowerCase()) ||
+        schedule.bookingEmail
+          .toLowerCase()
+          .includes(bookingNameEmail.toLowerCase())
+      );
+    });
   }
 
-  return schedules;
+  //filter eventStatus
+  if (eventStatus) {
+    result = result.filter((schedule) => {
+      if (eventStatus === 'Upcoming') {
+        return schedule.eventStartTime >= getCurrentDateTime();
+      } else if (eventStatus === 'Past') {
+        return schedule.eventStartTime < getCurrentDateTime();
+      }
+    });
+  }
+
+  return result;
 });
 
 const resetFilter = () => {
   selectedEventCategoryName.value = 'All';
   selectedDate.value = '';
   filterBookingNameEmail.value = '';
+  selectedEventStatus.value = 'All';
 };
 
 const getSchedulesData = async () => {
@@ -143,26 +159,36 @@ onBeforeMount(async () => {
     <div class="bg-white rounded-3xl h-2/3 w-3/12 flex shadow-lg px-2.5">
       <div class="flex flex-col p-10 min-w-full overflow-auto clinic-scollbar">
         <p class="text-gray-400 text-sm md:text-lg lg:text-2xl">Event Filter</p>
-        <Divider  text="Select Date" />
-      
+
+        <Divider text="Select Date" />
         <litepie-datepicker
           :formatter="formatter"
           as-single
           v-model="selectedDate"
         ></litepie-datepicker>
+
         <Divider text="Select Category" />
-    
         <select
           v-model="selectedEventCategoryName"
-          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+          class="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
         >
           <option selected>All</option>
           <option v-for="(value, index) in categoriesData" :key="index">
             {{ value.eventCategoryName }}
           </option>
         </select>
+
+        <Divider text="Event Status" />
+        <select
+          v-model="selectedEventStatus"
+          class="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+        >
+          <option v-for="(value, index) in allEventStatus" :key="index">
+            {{ value }}
+          </option>
+        </select>
+
         <Divider text="Other" />
-      
         <form>
           <label
             class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300"
@@ -177,12 +203,13 @@ onBeforeMount(async () => {
             <input
               type="text"
               v-model="filterBookingNameEmail"
-              class="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              class="block p-4 pl-10 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="Search Booking Name, Booking Email..."
               required
             />
           </div>
         </form>
+
         <Divider text="Reset" />
         <SmButton text="Reset" btnType="danger" @click="resetFilter" />
       </div>
