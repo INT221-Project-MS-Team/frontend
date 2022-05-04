ce601aa90efa5e7aa93545e6368ba7b3a545880d
siraom15 <siriwatjaiyungyuen@gmail.com>
2022-05-05 05:23:45 +0700

    Add ReserveFinish

---
+++ src/components/ReserveFinish.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { CheckCircleIcon } from '@heroicons/vue/outline';
+import Button from './Button.vue';
+import SmButton from './SmButton.vue';
+const props = defineProps({
+  reserveData: {
+    type: Object,
+    default: {},
+  },
+});
+</script>
+
+<template>
+  <div class="flex flex-col justify-center items-center gap-2 mt-16">
+    <CheckCircleIcon class="h-4/6 w-4/6 text-green-500" />
+    <p class="text-lg">Reserve Successfully</p>
+    <router-link :to="`/event?id=${reserveData.id}`">
+    <SmButton text="View" />
+
+    </router-link>
+  </div>
+</template>
+
+<style scoped></style>
