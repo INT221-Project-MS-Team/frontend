028f7332e8fee7853abf422aff8299c148dfc71c
siraom15 <siriwatjaiyungyuen@gmail.com>
2022-05-05 05:23:53 +0700

    Add ReserveCategoryForm

---
+++ src/components/ReserveCategoryForm.vue
@@ -0,0 +1,24 @@
+<script setup>
+import CategoryButton from './CategoryButton.vue';
+
+const emits = defineEmits(['selectCategory']);
+const props = defineProps({
+  categories: {
+    type: Array,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div>
+    <div class="text-sm md:text-base lg:text-xl text-center text-gray-900">
+      Select Category
+    </div>
+    <div class="grid grid-cols-2 gap-4 mt-10">
+        <CategoryButton :text="category.eventCategoryName" v-for="(category, index) in categories" :key="index" @click="$emit('selectCategory', index)" />
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
