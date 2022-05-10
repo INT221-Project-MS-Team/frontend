b94293b24f2266092a2ad9cff4dd9424ea2e6777
siraom15 <siriwatjaiyungyuen@gmail.com>
2022-05-10 17:40:06 +0700

    Add Badge Component

---
+++ src/components/Badge.vue
@@ -0,0 +1,33 @@
+<script setup>
+const props = defineProps({
+  text: {
+    type: String,
+    default: '',
+  },
+  color: {
+    type: String,
+    default: 'primary',
+  },
+});
+
+const colors = {
+  primary: ['bg-blue-100', 'text-blue-800'],
+  dark: ['bg-gray-100 ', 'text-gray-800'],
+  red: ['bg-red-100 ', 'text-red-800'],
+  green: ['bg-green-100 ', 'text-green-800'],
+  yellow: ['bg-yellow-100 ', 'text-yellow-800'],
+  indego: ['bg-indigo-100 ', 'text-indigo-800'],
+  purple: ['bg-purple-100 ', 'text-purple-800'],
+  pink: ['bg-pink-100 ', 'text-pink-800'],
+};
+</script>
+
+<template>
+  <span
+    class="text-xs font-semibold mr-2 px-2.5 py-0.5 rounded"
+    :class="colors[color]"
+    >{{ text }}</span
+  >
+</template>
+
+<style scoped></style>
