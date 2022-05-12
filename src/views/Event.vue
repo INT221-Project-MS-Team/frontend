a3ed05bd31eb63cf5c830903d9d01fac79c6e7fc
siraom15 <siriwatjaiyungyuen@gmail.com>
2022-05-12 20:42:38 +0700

    Update Change input to textarea

--- src/views/Event.vue
+++ src/views/Event.vue
@@ -283,12 +283,13 @@ onBeforeMount(async () => {
               >
                 {{ eventData.eventNotes || '(blank)' }}</span
               >
-              <input
-                type="text"
+              <textarea
                 v-model="editingEventNotes"
                 class="block w-full p-3 text-gray-900 border border-gray-300 rounded-lg bg-white sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 v-else
-              />
+                maxlength="500"
+                rows="4"
+              ></textarea>
 
               <span class="mt-1" v-if="isEditing">
                 <Divider text="Danger Zone" />
