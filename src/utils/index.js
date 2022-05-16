11288eaecdbc31b6acdba0069bac61ef99510484
siraom15 <siriwatjaiyungyuen@gmail.com>
2022-05-16 10:11:50 +0700

    Add Input Date Time Utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -6,16 +6,26 @@ export const convertDateFormat = (date) => {
   return date.split('-').reverse().join('/');
 };
 
-export const getDate = (eventStartTime) => {
-  let date = new Date(eventStartTime);
+export const getDate = (datetime) => {
+  let date = new Date(datetime);
   return date.toLocaleDateString('en-GB');
 };
 
-export const getTime = (eventStartTime) => {
-  let date = new Date(eventStartTime);
+export const getTime = (datetime) => {
+  let date = new Date(datetime);
   let option = { hour: '2-digit', minute: '2-digit' };
   return date.toLocaleTimeString('en-GB', option);
 };
+
+export const getInputDate = (datetime) =>{
+  let date = new Date(datetime);
+  return date.getFullYear() + '-' + ('0' + (date.getMonth() + 1)).slice(-2) + '-' + ('0' + date.getDate()).slice(-2);
+}
+
+export const getInputTime = (datetime) =>{
+  let date = new Date(datetime);
+  return ('0' + date.getHours()).slice(-2) + ':' + ('0' + date.getMinutes()).slice(-2);
+}
 
 export const convertDateTimeToISOString = (date, time) => {
   let dateTime = new Date(date + ' ' + time);
@@ -32,6 +42,20 @@ export const categoryIdToBadgeColor = (categoryId) => {
     'indego',
     'green',
     'dark',
+  ];
+  return categoryId - 1 > colors.length ? 'primary' : colors[categoryId - 1];
+};
+
+export const categoryIdToStyleColor = (categoryId) => {
+  let colors = [
+    ['bg-blue-100', 'text-blue-800'],
+    ['bg-yellow-100', 'text-yellow-800'],
+    ['bg-pink-100', 'text-pink-800'],
+    ['bg-purple-100', 'text-purple-800'],
+    ['bg-red-100', 'text-red-800'],
+    ['bg-indigo-100', 'text-indigo-800'],
+    ['bg-green-100', 'text-green-800'],
+    ['bg-gray-100', 'text-gray-800'],
   ];
   return categoryId - 1 > colors.length ? 'primary' : colors[categoryId - 1];
 };
@@ -62,16 +86,18 @@ export const isOverlapTime = (insertEvent, allEvents) => {
   let insertEventStart = new Date(insertEvent.eventStartTime);
   let insertEventEnd = new Date(insertEvent.eventStartTime);
   let insertEventId = insertEvent?.id ? insertEvent.id : -1;
+  console.log(insertEventId);
   insertEventEnd.setMinutes(
     insertEventEnd.getMinutes() + insertEvent.eventDuration
   );
   allEvents
     .filter(
       (event) =>
-        event.eventCategory.id === insertEvent.eventCategoryId &&
-        event.id !== insertEventId
+        event.eventCategory.id == insertEvent.eventCategoryId &&
+        event.id != insertEventId
     )
     .forEach((event) => {
+      console.log(event);
       let eventDateStart = new Date(event.eventStartTime);
       let eventDateEnd = new Date(event.eventStartTime);
       eventDateEnd.setMinutes(eventDateEnd.getMinutes() + event.eventDuration);
