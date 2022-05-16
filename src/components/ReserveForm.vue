78ccf75df2d0b6c461e9ad83ef632f6927cd7522
siraom15 <siriwatjaiyungyuen@gmail.com>
2022-05-16 10:42:33 +0700

    Update Form

--- src/components/ReserveForm.vue
+++ src/components/ReserveForm.vue
@@ -1,7 +1,7 @@
 <script setup>
 import Divider from '../components/Divider.vue';
 import { ref } from '@vue/reactivity';
-import { getDate, getTime } from '../utils/index';
+import { getDate, getTime, getInputDate } from '../utils/index';
 import SmButton from './SmButton.vue';
 import { computed } from '@vue/runtime-core';
 const emits = defineEmits(['back', 'next']);
@@ -72,6 +72,7 @@ const reserverInformation = computed(() => ({
             class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-100 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
             placeholder=" "
             required=""
+            :min="getInputDate(new Date())"
           />
           <label
             class="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
