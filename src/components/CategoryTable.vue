efe707a300030a677195ca895b93825e47cfe3ac
siraom15 <siriwatjaiyungyuen@gmail.com>
2022-05-19 00:59:12 +0700

    Update Event List

---
+++ src/components/CategoryTable.vue
@@ -0,0 +1,59 @@
+<script setup>
+import SmButton from './SmButton.vue';
+const props = defineProps({
+  categories: {
+    type: Array,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div
+    class="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1 flex-col rounded-lg clinic-scollbar overflow-auto"
+  >
+    <table
+      class="h-4/6 sm:rounded-lg text-sm text-left text-gray-500 dark:text-gray-400 "
+    >
+      <thead
+        class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400"
+      >
+        <tr>
+          <th scope="col" class="px-6 py-3">#</th>
+          <th scope="col" class="px-6 py-3">Name</th>
+          <th scope="col" class="px-6 py-3">Duration</th>
+          <th scope="col" class="px-6 py-3">Description</th>
+          <th scope="col" class="px-6 py-3"></th>
+        </tr>
+      </thead>
+      <tbody class="relative overflow-auto">
+        <tr
+          v-for="(category, index) in categories"
+          :key="index"
+          class="bg-white border-b hover:bg-gray-50"
+        >
+          <th scope="col" class="px-6 py-3">
+            {{ index + 1 }}
+          </th>
+          <th scope="col" class="px-6 py-3">
+            {{ category.eventCategoryName }}
+          </th>
+          <th scope="col" class="px-6 py-3">
+            {{ category.eventDuration }}
+          </th>
+          <th
+            class="px-6 py-4"
+            :class="!category.eventCategoryDescription ? ['text-gray-300'] : ''"
+          >
+            {{ category.eventCategoryDescription || 'No Description' }}
+          </th>
+          <th class="px-6 py-3 text-clinic-blue-300">
+            <SmButton text="Edit" btnType="edit" />
+          </th>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<style scoped></style>
--- src/views/Category.vue
+++ src/views/Category.vue
@@ -1,64 +1,43 @@
 <script setup>
-import CategoryList from '../components/CategoryList.vue';
+import CategoryTable from '../components/CategoryTable.vue';
 import { onBeforeMount } from '@vue/runtime-core';
 import { ref } from 'vue';
 
 const categoriesData = ref([]);
+
 const getCategoriesData = async () => {
-    const response = await fetch(
-        import.meta.env.VITE_SERVER_URL + '/api/events-categories'
-    );
-    if (response.status === 200) {
-        const data = await response.json();
-        categoriesData.value = data;
-        console.log(data);
-    } else {
-        console.log('Fetch Category Error');
-    }
+  const response = await fetch(
+    import.meta.env.VITE_SERVER_URL + '/api/events-categories'
+  );
+  if (response.status === 200) {
+    const data = await response.json();
+    categoriesData.value = data;
+    console.log(data);
+  } else {
+    console.log('Fetch Category Error');
+  }
 };
 
 onBeforeMount(async () => {
-    await getCategoriesData()
-})
+  await getCategoriesData();
+});
 </script>
- 
+
 <template>
+  <div
+    class="bg-schedules w-screen h-screen bg-no-repeat bg-cover bg-center flex flex-wrap flex-col items-center justify-center gap-2"
+  >
     <div
-        class="bg-schedules w-screen h-screen bg-no-repeat bg-cover bg-center flex flex-wrap flex-col items-center justify-center gap-2">
-        <div class="w-10/12  h-4/6 bg-white overflow-auto flex relative shadow-md sm:rounded-lg clinic-scollbar">
-            <div class="text-center md:text-lg flex flex-col lg:text-lg">
-                <span class="text-gray-700 text-3xl p-3"> Category </span>
-            <div class="flex  overflow-auto force-overflow grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1 flex-col sm:rounded-lg overflow-auto clinic-scollbar p-3">
-                <table class=" h-4/6 sm:rounded-lg text-sm text-left  text-gray-500 dark:text-gray-400">
-                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                        <tr>
-                            <th scope="col" class="px-6 py-3">
-                                Name
-                            </th>
-                            <th scope="col" class="px-6 py-3">
-                                Duration
-                            </th>
-                            <th scope="col" class="px-6 py-3">
-                                Description
-                            </th>
-                            <th scope="col" class="px-6 py-3">
-
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <CategoryList :categories="categoriesData" />
-                    </tbody>
-                </table>
-            </div>
-            </div>
-
-
-        </div>
-
+      class="w-10/12 h-5/6 bg-white flex shadow-md sm:rounded-lg clinic-scollbar"
+    >
+      <div class="text-center md:text-lg flex flex-col lg:text-lg">
+        <span class="text-gray-600 text-2xl p-3 bg-clinic-blue-25">
+          Category List</span
+        >
+        <CategoryTable :categories="categoriesData" />
+      </div>
     </div>
-
+  </div>
 </template>
- 
-<style>
-</style>
+
+<style></style>
