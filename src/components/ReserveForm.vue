73e50bb08cba8a7c506530f69db52a8813db9eaa
bewburats <bew25442@gmail.com>
2022-10-10 12:24:42 +0700

    add file input

--- src/components/ReserveForm.vue
+++ src/components/ReserveForm.vue
@@ -127,6 +127,18 @@ const reserverInformation = computed(() => ({
           >
         </div>
       </div>
+
+      <div>
+        <label class="block mb-2 text-xs font-medium text-gray-500 dark:text-gray-300" for="small_size">Upload
+          file</label>
+        <input
+          class="block w-full text-xs text-gray-900 bg-gray-50 rounded-lg border border-gray-300 cursor-pointer 
+          dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+          aria-describedby="file_input_help" id="file_input"  type="file">
+        <p class="mt-1 mb-2 text-xs text-gray-500 dark:text-gray-300" id="file_input_help">SVG, PNG, JPG or GIF (MAX.
+          800x400px).</p>
+      </div>
+
       <div class="relative z-0 w-full mb-6 group">
         <label class="block mb-2 text-xs font-medium text-gray-500">Note</label>
         <textarea
