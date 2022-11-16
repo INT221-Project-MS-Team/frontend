5284f12b8b34523985cbc7d3473cf3cc77696cfb
siraom15 <siriwatjaiyungyuen@gmail.com>
2022-11-16 11:56:48 +0700

    Fix bug

--- src/views/SignOut.vue
+++ src/views/SignOut.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { inject, ref } from 'vue';
+import { inject, onBeforeMount, ref } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
 import { useStatusStore } from '../store/status';
 import AuthService from '@/msal/index';
@@ -13,31 +13,32 @@ const logoutWithMSAL = async () => {
   await instance.logout();
 };
 
-const localLogout = async () => {
+const localLogout = () => {
   localStorage.removeItem('access_token');
   localStorage.removeItem('refresh_token');
+  storeStatus.setIsLoggedIn(false);
+  storeStatus.setLoggedInUser(null);
 };
 
-if (storeStatus.isLoggedIn) {
-  if (storeStatus.loggedInUser.type === 'msal') {
-    await logoutWithMSAL();
-    storeStatus.setIsLoggedIn(false);
-    storeStatus.setLoggedInUser(null);
-  } else {
-    await localLogout();
-    storeStatus.setIsLoggedIn(false);
-    storeStatus.setLoggedInUser(null);
-    swal({
-      title: 'Logout Success',
-      text: 'See you again',
-      icon: 'success',
-      timer: 2000,
-      showConfirmButton: false,
-    });
-    router.push({ name: 'sign-in' });
+onBeforeMount(async () => {
+  if (storeStatus.isLoggedIn) {
+    if (storeStatus.loggedInUser?.type === 'msal') {
+      await logoutWithMSAL();
+      localLogout();
+    } else {
+      localLogout();
+      swal({
+        title: 'Logout Success',
+        text: 'You have been logged out',
+        icon: 'success',
+        timer: 2000,
+        showConfirmButton: false,
+      });
+      router.push({ name: 'sign-in' });
+    }
   }
-  
-}
+  router.push({ name: 'sign-in' });
+});
 </script>
 
 <template>
